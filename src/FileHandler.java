072ac0249907b37c0105c161536e99fb25a3ba8e
Silvan <silvan.lendi@gmx.ch>
2020-11-28 18:12:31 +0100

    made paths relative

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -6,26 +6,26 @@ public class FileHandler {
     private ObjectMapper objectMapper = new ObjectMapper();
 
     public void writeToFileVehicles(VehicleContainer v) throws IOException {
-        objectMapper.writeValue(new File("C:\\Module\\326\\LB1\\code\\files\\vehicleData.json"), v);
+        objectMapper.writeValue(new File("files/vehicleData.json"), v);
     }
 
     public void writeToFileCustomers(CustomerContainer c) throws IOException {
-        objectMapper.writeValue(new File("C:\\Module\\326\\LB1\\code\\files\\customerData.json"), c);
+        objectMapper.writeValue(new File("files/customerData.json"), c);
     }
 
     public void writeToFileRents(RentContainer r) throws IOException {
-        objectMapper.writeValue(new File("C:\\Module\\326\\LB1\\code\\files\\rentData.json"), r);
+        objectMapper.writeValue(new File("files/rentData.json"), r);
     }
 
     public VehicleContainer initializeReadFileVehicles() throws IOException {
-        return objectMapper.readValue(new File("C:\\Module\\326\\LB1\\code\\files\\vehicleData.json"), VehicleContainer.class);
+        return objectMapper.readValue(new File("files/vehicleData.json"), VehicleContainer.class);
     }
 
     public CustomerContainer initializeReadFileCustomers() throws IOException {
-        return objectMapper.readValue(new File("C:\\Module\\326\\LB1\\code\\files\\customerData.json"), CustomerContainer.class);
+        return objectMapper.readValue(new File("files/customerData.json"), CustomerContainer.class);
     }
 
     public RentContainer initializeReadFileRents() throws IOException {
-        return objectMapper.readValue(new File("C:\\Module\\326\\LB1\\code\\files\\rentData.json"), RentContainer.class);
+        return objectMapper.readValue(new File("files/rentData.json"), RentContainer.class);
     }
 }
